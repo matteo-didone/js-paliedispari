36706d08cd81d415d5a10c8d091815cb377816e2
matteo-didone <matteo.dido9@gmail.com>
2023-05-22 16:58:45 +0200

    Fixed bug about word-length

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ if(word == null || word == "" || word == " ")  //Check if the word is null, empt
 }
 
 //Now, I'm going to use a for loop to add a character to the array for each character in the word entered by the user
-for(let i = 0; i < originalWordArray.length; i++)
+for(let i = 0; i < word.length; i++)
 {
     originalWordArray.push(word.charAt(i)); //Add a character to the array for each character in the word entered by the user
 }
@@ -38,7 +38,7 @@ function isPalindrome(array = [], reversedArray = [])
             console.log("The string is palindrome"); //Print to the console if the word entered is a palindrome
             return true; //Return true if the character in the originalWordArray is equal to the character in the reversedWordArray
         }
-        else
+        else if (array[i] !== reversedArray[i]) //Check if the character in the originalWordArray is not equal to the character in the reversedWordArray
         {
             console.log("The string is not palindrome"); //Print to the console if the word entered is not a palindrome
             return false; //Return false if the character in the originalWordArray is not equal to the character in the reversedWordArray
@@ -51,4 +51,4 @@ let result = isPalindrome(originalWordArray, reversedWordArray);
 //Print the result to the console
 console.log(result);
 //Print the result to the HTML page
-document.getElementById("result-palindrome").innerHTML = result;
+document.getElementById("result-palindrome").innerHTML = result ? "The string is palindrome" : "The string is not palindrome";
